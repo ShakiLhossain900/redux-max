d97d9160b65d78ff2ae2b5c055a8eafa72e04386
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-21 23:01:06 +0600

    edding the counter logic

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,12 +2,16 @@ import {useSelector} from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
+
+ const counter = useSelector(state=> state.counter) //resive the state manage by redux 
+  //it's retuen the redux state
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}></div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
