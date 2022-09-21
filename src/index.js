6d89e258327f10cd43f10fb7866ac3b01db619b1
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-21 22:48:54 +0600

    adding the store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {Provider} from 'react-redux'
- 
-ReactDOM.render(<Provider><App/> </Provider>, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store/index";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
