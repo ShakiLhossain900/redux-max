eb5dde6c86798f722ba1be7e0da25339bec6e6a8
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-22 08:33:15 +0600

    toggle counter added using the react concept

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,27 +2,32 @@ import { useSelector, useDispatch } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const counter = useSelector((state) => state.counter); //resive the state manage by redux
   //it's retuen the redux state
 
- const incremnetHandler= () => {
-  dispatch({type: "increment",})
- }
- const increseHandler =() =>{
-  dispatch({type: "increase", amount:5})
- }
- const decrementHandler= () => {
-  dispatch({type: "decrement",})
- }
+  //i want to get  the value of show counter
+  const show = useSelector((state) => state.showCounter);
 
-  const toggleCounterHandler = () => {};
+  const incremnetHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const increseHandler = () => {
+    dispatch({ type: "increase", amount: 5 });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incremnetHandler}>Increment</button>
         <button onClick={increseHandler}>Increse by 5</button>
